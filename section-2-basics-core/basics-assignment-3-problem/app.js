7ac434d02b56686bb8185a5f8919b71798ffbee0
Caio Carvalho <caio.carvalho@lavid.ufpb.br>
2024-02-04 01:19:56 -0300

    Reactivity challenge

---
+++ section-2-basics-core/basics-assignment-3-problem/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        result: 0,
+      };
+    },
+    computed: {
+      resultStatus() {
+        if (this.result < 37) {
+          return "Not there yet";
+        } else if (this.result > 37) {
+          return "Too much!";
+        } else {
+          return "exactly 37";
+        }
+      },
+    },
+    watch: {
+      result(newValue, oldValue) {
+        if (newValue !== oldValue) {
+          setTimeout(() => {
+            this.result = 0;
+          }, 5000);
+        }
+      },
+    },
+    methods: {
+      addOne() {
+        this.result += 1;
+      },
+      addFive() {
+        this.result += 5;
+      },
+    },
+  });
+  
+  app.mount("#assignment");
+  
