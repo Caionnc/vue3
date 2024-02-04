e10674951bfa7c1e2e1fc5d7085a2f3d128076d4
Caio Carvalho <caio.carvalho@lavid.ufpb.br>
2024-02-04 00:12:01 -0300

    Vue dummy project

---
+++ my-vue-app/src/components/Watchers.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref, watch } from "vue";
+
+const question = ref("");
+const answer = ref("Questions usually contain a question mark. ;-)");
+const loading = ref(false);
+const x = ref(0);
+const y = ref(0);
+
+// single ref
+watch(x, (newX) => {
+  console.log(`x is ${newX}`);
+});
+
+// getter
+watch(
+  () => x.value + y.value,
+  (sum) => {
+    console.log(`sum of x + y is: ${sum}`);
+  }
+);
+
+// array of multiple sources
+watch([x, () => y.value], ([newX, newY]) => {
+  console.log(`x is ${newX} and y is ${newY}`);
+});
+
+// watch works directly on a ref
+watch(question, async (newQuestion, oldQuestion) => {
+  if (newQuestion.includes("?")) {
+    loading.value = true;
+    answer.value = "Thinking...";
+    try {
+      const res = await fetch("https://yesno.wtf/api");
+      answer.value = (await res.json()).answer;
+    } catch (error) {
+      answer.value = "Error! Could not reach the API. " + error;
+    } finally {
+      loading.value = false;
+    }
+  }
+});
+</script>
+
+<template>
+  <p>
+    Ask a yes/no question:
+    <input v-model="question" :disabled="loading" />
+  </p>
+  <p>{{ answer }}</p>
+</template>
